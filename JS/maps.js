0cda90424a111213f62c69e9ee5e6e1cedd34d7a
DenisD28 <dsalmerono14@gmail.com>
2026-06-30 14:25:29 -0600

    incorporacion de mapa

---
+++ JS/maps.js
@@ -0,0 +1,19 @@
+// function initMap() {
+    
+//   const map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 12.434764091085613, lng: -86.88995492777792},
+//     zoom: 17,
+//     mapId: 'DEMO_MAP_ID',
+//   });
+
+//   const marker = new google.maps.marker.AdvancedMarkerElement({
+//     map,
+//     position: {lat: 12.434764091085613, lng: -86.88995492777792},
+//   });
+
+//   marker.addListener('click', ({domEvent, latLng}) => {
+//     const {target} = domEvent;
+//     // Handle the click event.
+//     // ...
+//   });
+// }
