ae77778f66eb8afaa5d0c7af5c92dc5f2723538c
DenisD28 <dsalmerono14@gmail.com>
2026-06-30 12:49:11 -0600

    Se agrego un modal para la informacion de la moto y enlace a whatsapp

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,11 @@
-window.addEventListener('load', (event) => {
-    cargarMotos();
+window.addEventListener('load', () => {
+
+    if (window.location.pathname.includes('index.html')) {
+        cargarMotos();
+    }
+
 });
 
-const moto = {
-    Marca: "Honda",
-    Modelo: "Navi 110",
-    Descripcion: "Mini moto urbana con diseño juvenil.",
-    Año: 2026,
-    precio: "$2,450",
-    rutadeimg: "IMG/navieditada.png",
-    tipo: "Mini moto urbana"
-}
 
 function cargarMotos() {
     const contenedor = document.getElementById("contenedorMotos");
@@ -24,7 +19,7 @@ function cargarMotos() {
         })
 }
 
-function crearCardMoto(moto, index) {
+function crearCardMoto(moto) {
     return `
         <div class="col-md-4 col-lg-3 mb-4">
             <div class="card h-100 shadow">
@@ -43,13 +38,51 @@ function crearCardMoto(moto, index) {
                 </ul>
 
                 <div class="card-body">
-                    <button class="btn btn-outline-danger w-100"
-                            onclick="verMoto(${index})">
+                    <button type="button" class="btn btn-outline-danger w-100" data-bs-toggle="modal" data-bs-target="#exampleModal" 
+                            onclick="verMoto(${moto.id})">
                         Ver ficha técnica
                     </button>
                 </div>
             </div>
         </div>
     `;
+}
+
+function verMoto(id) {
+    const modalContainer = document.getElementById("fichaTecnica");
+
+    fetch('JSON/Motos.json')
+        .then(res => res.json())
+        .then(data => {
+            const moto = data.find(m => m.id === id);
+            modalContainer.innerHTML=mostrarModal(moto);
+        });
+}
+
+function mostrarModal(moto) {
+    console.log(moto);
+    
+    document.getElementById("exampleModalLabel").textContent = moto.Modelo;
+    document.getElementById("exampleModal").querySelector(".modal-body").innerHTML = `
+        <img src="${moto.rutadeimg}" class="img-fluid mb-3" alt="${moto.Modelo}">
+        <p><strong>Tipo:</strong> ${moto.tipo}</p>
+        <p><strong>Año:</strong> ${moto.Año}</p>
+        <p><strong>Precio:</strong> ${moto.precio}</p>
+        <p><strong>Precio Promoción Contado:</strong> ${moto.precioPromo}</p>
+        <p><strong>Descripción:</strong> ${moto.Descripcion}</p>
+        <p><strong>Ficha Técnica:</strong></p>
+        <p><strong>Motor:</strong>${moto.Caracteristicas.Motor}</p>
+        <p><strong>Potencia:</strong>${moto.Caracteristicas.Potencia}</p>
+        <p><strong>Transmisión:</strong>${moto.Caracteristicas.Transmision}</p>
+        <p><strong>Arranque:</strong>${moto.Caracteristicas.Arranque}</p>
+        <p><strong>FrenosDelantero:</strong>${moto.Caracteristicas.FrenoDelantero}</p>
+        <p><strong>FrenosTrasero:</strong>${moto.Caracteristicas.FrenoTrasero}</p>
+        <p><strong>Capacidad de Tanque:</strong>${moto.Caracteristicas.CapacidadTanque}</p>
+    `;
+}
+
+
+function redireccion(){
+    window.location.href = "https://wa.link/2atmxs";
 }
 
